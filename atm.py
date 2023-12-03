d2d0b89efd4ae094660b6a4ef3e321454ced2921
timjgar <122241906+timjgar@users.noreply.github.com>
2023-12-02 16:50:54 -0800

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,10 @@
+class Atm(object):
+  def __init(self,cardnumber,pin):
+    self.cardnumber=cardnumber
+    self.pin=pin
+  def check_balance(self):
+    print("Your balance is 50,000")
+  def CashWithdrawal(self):
+    print("Your have withdrawn 50,000")
+
+Timothy = Atm("Timothy", 12345, 54321)
